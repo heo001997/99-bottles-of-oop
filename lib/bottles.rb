fb88a2d1a3ced493c9ed13522acf2966dfff0003
heo001997 <heo001997@gmail.com>
2023-12-01 23:10:36 +0700

    Style 2 - Duplicate String to Constant

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,4 +1,8 @@
 class Bottles
+  BEER_WALL = "of beer on the wall"
+  BEER_DOWN = "down and pass it around"
+  BEER = "of beer"
+
   def verse(total_bottles)
     case total_bottles
     when 2
@@ -31,22 +35,22 @@ class Bottles
   private
 
   def bottles_99_to_3(total_bottles)
-    "#{total_bottles} bottles of beer on the wall, #{total_bottles} bottles of beer.\n" \
-      "Take one down and pass it around, #{total_bottles - 1} bottles of beer on the wall.\n"
+    "#{total_bottles} bottles #{BEER_WALL}, #{total_bottles} bottles #{BEER}.\n" \
+      "Take one #{BEER_DOWN}, #{total_bottles - 1} bottles #{BEER_WALL}.\n"
   end
 
   def bottles_2_to_1
-    "2 bottles of beer on the wall, 2 bottles of beer.\n" \
-      "Take one down and pass it around, 1 bottle of beer on the wall.\n"
+    "2 bottles #{BEER_WALL}, 2 bottles #{BEER}.\n" \
+      "Take one #{BEER_DOWN}, 1 bottle #{BEER_WALL}.\n"
   end
 
   def bottles_1_to_0
-    "1 bottle of beer on the wall, 1 bottle of beer.\n" \
-      "Take it down and pass it around, no more bottles of beer on the wall.\n"
+    "1 bottle #{BEER_WALL}, 1 bottle #{BEER}.\n" \
+      "Take it #{BEER_DOWN}, no more bottles #{BEER_WALL}.\n"
   end
 
   def bottles_0
-    "No more bottles of beer on the wall, no more bottles of beer.\n" \
-      "Go to the store and buy some more, 99 bottles of beer on the wall.\n"
+    "No more bottles #{BEER_WALL}, no more bottles #{BEER}.\n" \
+      "Go to the store and buy some more, 99 bottles #{BEER_WALL}.\n"
   end
 end
